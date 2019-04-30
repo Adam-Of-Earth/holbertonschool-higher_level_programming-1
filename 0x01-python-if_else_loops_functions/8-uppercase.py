d740773cb9ae1d5257cc9bb6ceedc28835eea5c0
zmbslyr <mhedgeland@gmail.com>
2019-04-30 17:24:33 +0000

    Simplified to get rid of 'chr'

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
 def uppercase(str):
-    g = ""
-    for i in str:
-        if ord(i) >= 97 and ord(i) <= 122:
-            x = ord(i) - 32
-            y = chr(x)
-            g = g + y
-        else:
-            j = i
-            g = g + j
-    print(g)
+    for ch in str:
+        i = ord(ch)
+        print("{:c}".format(i - 32 if 97 <= i <= 122 else i), end="")
+    print()
