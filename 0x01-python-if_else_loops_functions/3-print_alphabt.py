78c0b44eb97859f34d716cb16f8835e5162294d4
zmbslyr <mhedgeland@gmail.com>
2019-04-30 16:50:35 +0000

    Removed chr

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(26):
-    if i == 4 or i == 16:
+for i in range(97, 123):
+    if i == 101 or i == 113:
         print("", end="")
     else:
-        print(chr(97 + i), end="")
+        print("{:c}".format(i), end="")
