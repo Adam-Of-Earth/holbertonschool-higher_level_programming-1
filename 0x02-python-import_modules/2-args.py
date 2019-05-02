6e815fb9fc5df686c9604bd0640d3a9cff6a47ae
zmbslyr <mhedgeland@gmail.com>
2019-05-02 17:22:43 +0000

    More robust print statements

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    print("{:d} arguments:".format(len(sys.argv) - 1))
+    if len(sys.argv) == 2:
+        print("{:d} argument:".format(len(sys.argv) - 1))
+    else:
+        print("{:d} arguments:".format(len(sys.argv) - 1))
     for i in range(1, len(sys.argv)):
         print("{:d}: {:s}".format(i, sys.argv[i]))
