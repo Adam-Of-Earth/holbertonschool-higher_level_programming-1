81e4b226b26cb97f6414b3dacec76d8d50b3cebb
zmbslyr <mhedgeland@gmail.com>
2019-05-02 17:26:10 +0000

    Added case for no arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import sys
-    if len(sys.argv) == 2:
+    if len(sys.argv) == 1:
+        print("{:d} arguments.".format(len(sys.argv) - 1))
+    elif len(sys.argv) == 2:
         print("{:d} argument:".format(len(sys.argv) - 1))
     else:
         print("{:d} arguments:".format(len(sys.argv) - 1))
