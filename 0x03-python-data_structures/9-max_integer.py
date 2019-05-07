97a156020aedf1642022574aad3c86883dc0e914
zmbslyr <mhedgeland@gmail.com>
2019-05-07 19:53:16 +0000

    Update to handle empty lists

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
-        return None
-    x = 0
-    for i in my_list:
-        if i > x:
-            x = i
-    return x
+    if len(my_list) == 0 or my_list is None:
+        return
+    else:
+        x = 0
+        for i in my_list:
+            if i is None:
+                return
+            if i > x:
+                x = i
+        return x
