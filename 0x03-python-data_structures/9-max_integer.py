e18f57c5865df5b1021f7b300b35128be1c5067e
zmbslyr <mhedgeland@gmail.com>
2019-05-07 20:33:53 +0000

    Changed 'if' logic

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0 or my_list[0] is None:
-        return None
-    else:
-        print(len(my_list))
+    if len(my_list) != 0:
         x = 0
         for i in my_list:
             if i > x:
