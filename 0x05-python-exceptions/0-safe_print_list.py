b5799b6e099ea76f954bd7fb6e90d8c37a049e36
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-05-20 14:26:26 -0400

    Testing 'try' and 'except'

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
+    a = 0
+    for i in my_list:
+        try:
+            if a < x:
+                print(i, end="")
+                a += 1
+        except IndexError:
+            pass
+    print()
+    return a
