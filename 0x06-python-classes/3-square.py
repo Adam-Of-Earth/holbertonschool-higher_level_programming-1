da7fbaaf54029e4aeacd5c4a286a5935f4f8ace4
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-05-21 19:23:46 -0400

    Added method to find the area of the square

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+""" Square Class
+
+Creates and manupulates a square object
+"""
+
+
+class Square:
+    """ Geometric Square
+
+    A geometric shape with four equal sides, and methods
+    for manipulation
+    """
+    def __init__(self, size=0):
+        """Initializes a square at a given size
+
+        The size of the new square instance is hidden
+
+        Args:
+            size (int): length of the square's sides
+
+        Raises:
+            TypeError: size argument isnt an integer
+            ValueError: size argument is negative
+        """
+        if type(size) != int:
+            raise(TypeError("size must be an integer"))
+        elif size < 0:
+            raise(ValueError("size must be >= 0"))
+        else:
+            self.__size = size
+    def area(self):
+        """Finds the area of the square from the size given
+
+        Returns:
+            size attribute squared
+        """
+        return self.__size ** 2
