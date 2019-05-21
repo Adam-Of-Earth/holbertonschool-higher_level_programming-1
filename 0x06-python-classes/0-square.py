c904710aac46f3e81051b0e22095f736bdea8644
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-05-21 12:58:55 -0400

    Empty Square class

---
+++ 0x06-python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+""" Square Class
+
+Creates and manupulates a square object
+"""
+class Square:
+    """ Geometric Square
+
+    A geometric shape with four equal sides, and methods 
+    for manipulation
+    """
+    pass
