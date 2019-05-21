033e2525cc352cc561f65675c0671f043f16397f
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-05-20 23:19:39 -0400

    Prints a list of ints

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,2 +1,14 @@
 #!/usr/bin/python3
 def safe_print_list_integers(my_list=[], x=0):
+    ret, index = 0, 0
+    while index < x:
+        try:
+            print("{:d}".format(my_list[index]), end="")
+        except (TypeError, ValueError):
+            pass
+        else:
+            ret += 1
+        finally:
+            index += 1
+    print()
+    return ret
