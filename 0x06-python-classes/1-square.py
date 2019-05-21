bad0ed69e1a389c7caf3aa860c45bec7882074cd
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-05-21 13:30:52 -0400

    Gives Square class a size

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+""" Square Class
+
+Creates and manupulates a square object
+
+"""
+
+
+class Square:
+    """ Geometric Square
+
+    A geometric shape with four equal sides, and methods
+    for manipulation
+    """
+    def __init__(self, size):
+        """Initializes a square"""
+        self.__size = size
