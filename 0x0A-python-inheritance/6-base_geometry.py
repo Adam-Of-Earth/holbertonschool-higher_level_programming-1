4c00b10ce55d3d7e4b5367425adc269059440815
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-06-05 14:31:05 -0400

    Adds unimplemented method to BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Class with unimplementated method"""
+
+
+class BaseGeometry:
+    """Geometry class"""
+
+    def area(self):
+        """Not implemented
+
+        Raises:
+            Exception: Not implemented
+        """
+        raise(Exception("area() is not implemented"))
