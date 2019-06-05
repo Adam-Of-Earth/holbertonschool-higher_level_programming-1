39444676b1539d9fbdbd152e12c091ba99537810
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-06-05 13:14:51 -0400

    Added documentation

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
+"""Function to get attributes and methods of a Class"""
+
 def lookup(obj):
+    """Return attributes and methods available in object
+
+    Args:
+        obj: object to search
+    """
     return dir(obj)
