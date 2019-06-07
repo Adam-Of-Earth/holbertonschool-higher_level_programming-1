6ef484594111c677daa0836639e2f5a69da7df21
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-06-06 21:52:30 -0400

    Returns dictionary description of python obj

---
+++ 0x0B-python-input_output/10-class_to_json.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Module to get dictionary description of Python object"""
+
+
+def class_to_json(obj):
+    """Function to get dictionary description
+
+    Args:
+        obj: Object to get dictionary description of
+
+    Returns:
+        dic: Dictionary representation of a Python object
+    """
+    lst = obj.__dict__
+    dic = {}
+    for key, value in lst.items():
+        if isinstance(value, list) or isinstance(value, dict) or isinstance(
+            value, str) or isinstance(value, int) or isinstance(value, bool):
+            dic.update({key: value})
+    return dic
