33cc3a9c38b64d98b5673fa555ebb8fe05d5adfd
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-06-06 20:53:43 -0400

    Writes to a file

---
+++ 0x0B-python-input_output/3-write_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""Module to write to a file"""
+
+
+def write_file(filename="", text=""):
+    """Function to write to a file
+
+    Args:
+        filename: Path to file to write to
+        text: Text to write to file
+
+    Return:
+        int: Number of characters written
+    """
+    with open(filename, encoding="UTF-8") as myFile:
+        myFile.write(text)
+    return len(text)
