85aee93ac97edfe6847542c8d6100cfe7b91b735
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-06-12 12:56:06 -0400

    Update to getter/setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -17,30 +17,26 @@ class Square(Rectangle):
             y: Y position
             id: ID of the object created
         """
-        self.size = size
         super().__init__(size, size, x, y, id)
 
     @property
     def size(self):
         """Getter method for size"""
 
-        return self.__size
+        return self.width
 
     @size.setter
     def size(self, value):
         """Validator setter method for size"""
 
-        if type(value) is not int:
-            raise(TypeError("width must be an integer"))
-        if value <= 0:
-            raise(ValueError("width must be > 0"))
-        self.__size = value
+        self.width = value
+        self.height = value
 
     def __str__(self):
         """Method to return string representation of Square"""
 
         return "[Square] ({}) {}/{} - {}".format(
-            self.id, self.x, self.y, self.__size)
+            self.id, self.x, self.y, self.height)
 
     def update(self, *args, **kwargs):
         """Method to assign arguments to attributes
