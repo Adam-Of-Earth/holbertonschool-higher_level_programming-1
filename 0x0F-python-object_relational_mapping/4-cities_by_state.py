f32a2856e8dee0cbcfa5d2438469da89940a45a3
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-11 23:28:52 -0400

    [GLOBAL][py] Lists all cities from an SQL database

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,1 +1,27 @@
 #!/usr/bin/python3
+"""Module to list all cities from a SQL database"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    user = argv[1]
+    pwd = argv[2]
+    dbname = argv[3]
+    dbase = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=user,
+                            pwd=pwd,
+                            db=dbname)
+
+    cursor = dbase.cursor()
+    cursor.execute(
+        "SELECT cities.id, cities.name, states.name \
+        FROM cities INNER JOIN states ON states.id = cities.state_id \
+        ORDER BY cities.id ASC;")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    dbase.close()
