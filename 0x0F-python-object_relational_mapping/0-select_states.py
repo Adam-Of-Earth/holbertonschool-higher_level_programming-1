d7b58f6429712afec34ce902796a4a5bafa17856
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-11 23:12:25 -0400

    [GLOBAL][py] Initial Commit

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Script to lists all states form a db"""
+
+
+if __name__ == "__main__":
+    import MySQLdb
+    from sys import argv
+
+    user = argv[1]
+    pwd = argv[2]
+    dbname = argv[3]
+
+    dbase = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=user,
+                               passwd=pwd,
+                               db=dbname)
+    cursor = dbase.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print("{}".format(row))
+    cursor.close()
+    dbase.close()
