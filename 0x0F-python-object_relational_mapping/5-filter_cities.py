bf66b7a02fb92339d71a9f29a43a428784c127c8
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-11 23:33:43 -0400

    [GLOBAL][py] Lists all cities in a db that match argument

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,1 +1,30 @@
 #!/usr/bin/python3
+"""module to list all cities from a db that match argument"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    user = argv[1]
+    pwd = argv[2]
+    dbname = argv[3]
+    sname = argv[4]
+    dbase = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=user,
+                            passwd=pwd,
+                            db=dbname)
+
+    cursor = dbase.cursor()
+    cursor.execute(
+        "SELECT cities.name FROM cities \
+        INNER JOIN states ON states.id = cities.state_id \
+        WHERE states.name = %s;", (sname,))
+    rows = cursor.fetchall()
+    s = []
+    for row in rows:
+        s.append(row[0])
+    print(', '.join(s))
+    cursor.close()
+    dbase.close()
