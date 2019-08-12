daee43e05f58b4ae0620fd1edfe1b430446b916e
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-11 23:18:43 -0400

    [GLOBAL][py] Filters states out of an sql table

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Script to filter states from an SQL table"""
+
+
+from sys import argv
+import MySQLdb
+
+
+if __name__ == '__main__':
+    user = argv[1]
+    pwd = argv[2]
+    dbname = argv[3]
+
+    dbase = MySQLdb.connect(host="localhost",
+                               port=3306,
+                               user=user,
+                               passwd=pwd,
+                               db=dbname)
+    cursor = dbase.cursor()
+    cursor.execute("SELECT * FROM states WHERE name \
+    LIKE BINARY 'N%' ORDER BY id ASC")
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    dbase.close()
