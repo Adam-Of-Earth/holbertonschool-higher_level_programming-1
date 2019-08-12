1e6fb5a9819737a100c53819512ab775842aad1d
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-11 23:26:05 -0400

    [GLOBAL][py] Filters states by user safely

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,1 +1,26 @@
 #!/usr/bin/python3
+"""Module to filter states by user"""
+
+
+if __name__ == "__main__":
+    from sys import argv
+    import MySQLdb
+
+    user = argv[1]
+    pwd = argv[2]
+    dbname = argv[3]
+    sname = argv[4]
+    dbase = MySQLdb.connect(host="localhost",
+                            port=3306,
+                            user=user,
+                            pwd=pwd,
+                            db=dbname)
+
+    cursor = dbase.cursor()
+    cursor.execute(
+        "SELECT * FROM states WHERE name = %s ORDER BY id ASC", (sname,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    dbase.close()
