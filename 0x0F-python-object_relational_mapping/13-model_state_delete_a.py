e6fa99e98d065947cede559cb54f14203789e876
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-11 23:53:53 -0400

    [GLOBAL][py] Lists all states containing an 'a' char

--- 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -1,1 +1,22 @@
 #!/usr/bin/python3
+"""Module to list all states that contain an a"""
+
+
+from sqlalchemy.orm import sessionmaker
+from sys import argv
+from sqlalchemy import create_engine
+from model_state import Base, State
+
+
+if __name__ == '__main__':
+    engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}".format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    states = session.query(State).filter(
+        State.name.contains('a')).delete(
+        synchronize_session=False)
+    session.flush()
+    session.commit()
+    session.close()
