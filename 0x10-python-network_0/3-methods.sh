29ddeea0123545d8483d08681cba463dfbcbb9dc
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-08-20 20:55:00 -0400

    [GLOBAL][bash] Another typo fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept
-curl -is -X OPTIONS "$1" | grep 'Allow' | cut -d ' ' -f 2
+curl -is -X OPTIONS "$1" | grep 'Allow' | cut -d ' ' -f 2-
