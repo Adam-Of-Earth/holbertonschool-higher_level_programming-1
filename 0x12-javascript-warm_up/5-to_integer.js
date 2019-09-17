b0ec0ca55d1141a712ff9852f9e21ab20549ad6b
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-17 12:28:31 -0400

    [GLOBAL][js] Trying different way to get rid of float

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -3,5 +3,5 @@ const process = require('process');
 if (isNaN(process.argv[2])) {
   console.log('Not a Number');
 } else {
-  console.log(`My number: ${parseInt(process.argv[2])}`);
+  console.log(`My number: ${math.floor(process.argv[2])}`);
 }
