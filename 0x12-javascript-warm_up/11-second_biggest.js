a83e26f09694546fa2c08c269b5e796c1f022937
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-17 21:10:52 -0400

    [GLOBAL][js] Fixed wrong variable reference

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -6,7 +6,7 @@ if (process.argv[2] === undefined) {
   console.log(0);
 } else {
   const arr = [];
-  for (let index = len; i > 2; i--) {
+  for (let index = len; index > 2; index--) {
     arr.push(parseInt(process.argv[index - 1]));
   }
   arr.sort((left, right) => left - right);
