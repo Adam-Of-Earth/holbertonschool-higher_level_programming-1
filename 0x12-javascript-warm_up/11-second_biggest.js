a8e8e002e6bf047d61e69fe1051a50fc84ffbdce
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-17 21:06:19 -0400

    [GLOBAL][js] Finds the second largest number

---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const len = process.argv.length;
+if (process.argv[2] === undefined) {
+  console.log(0);
+} else if (len === 3) {
+  console.log(0);
+} else {
+  const arr = [];
+  for (let index = len; i > 2; i--) {
+    arr.push(parseInt(process.argv[index - 1]));
+  }
+  arr.sort((left, right) => left - right);
+  const ret = len - 4;
+  console.log(arr[ret]);
+}
