75dc01a7dd839f066e43dcb5ae46c9dc796dc7d0
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-17 21:00:08 -0400

    [GLOBAL][js] Slight change

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const process = require('process');
-if (isNaN(process.argv[2])) {
+const num = Number(process.argv[2]);
+if (isNaN(num)) {
   console.log('Not a Number');
 } else {
-  console.log(`My number: ${math.floor(process.argv[2])}`);
+  console.log('My number: ' + Math.floor(num));
 }
