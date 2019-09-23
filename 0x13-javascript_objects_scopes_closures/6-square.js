451f36e335f1e125a0b644f5c6f4a85333f33097
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-23 14:27:50 -0400

    [GLOBAL][js] Adds charPrint method to square class

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+module.exports = class Square extends require('./5-square.js') {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'x';
+    }
+    const row = c.repeat(this.width);
+    for (let index = 0; index < this.height; index++) {
+      console.log(row);
+    }
+  }
+};
