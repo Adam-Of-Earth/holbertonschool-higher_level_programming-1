78bb781f205fc1d68dc1d19119487c399e44b911
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-23 19:04:22 -0400

    [GLOBAL][js] Fixed missing semi-colon

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const list = require('./100-data').list;
-const mp = list.map(function (current, index) { return (current * index); })
+const mp = list.map(function (current, index) { return (current * index); });
 console.log(list);
 console.log(mp);
