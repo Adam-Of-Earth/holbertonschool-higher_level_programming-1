a79efe076d9df2200cbb27ca440cefb11fbbfd38
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-23 18:31:12 -0400

    [GLOBAL][js] Semistandard changes

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, searchElement) {
   let count = 0;
-  let len = list.length;
+  const len = list.length;
   for (let index = 0; index < len; index++) {
     if (list[index] === searchElement) {
       count++;
     }
   }
   return (count);
-}
+};
