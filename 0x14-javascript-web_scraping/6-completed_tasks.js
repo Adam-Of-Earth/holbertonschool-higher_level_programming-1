1620ab0a12907ed7f8b11a1a1a4c146bb9389631
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-24 14:02:32 -0400

    [GLOBAL][js] Shows number of tasks completed by a user

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+const request = require('request');
+request.get(process.argv[2], { json: true }, function (error, response, body) {
+  if (error) {}
+  const ret = {};
+  for (const task of body) {
+    if (task.completed === true) {
+      if (ret[task.userId] === undefined) {
+        ret[task.userId] = 0;
+      }
+      ret[task.userId]++;
+    }
+  }
+  console.log(ret);
+});
