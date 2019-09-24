7c63f683351c653deecf034d493f490cd4a9cec3
Mark Hedgeland <markhedgeland@Marks-MacBook-Air.local>
2019-09-24 15:26:56 -0400

    [GLOBAL][js] Finds all characters from a Star Wars film

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const request = require('request');
+const url = 'http://swapi.co/api/films/' + process.argv[2];
+request(url, function (error, response, body) {
+  if (error) { return; }
+  const characterList = JSON.parse(body).characters;
+  for (let index = 0; index < characterList.length; index++) {
+    request(characterList[index], function (error, response, body) {
+      if (error) { return; }
+      console.log(JSON.parse(body).name);
+    });
+  }
+});
